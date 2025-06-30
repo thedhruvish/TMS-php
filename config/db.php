875c1beb30306cc307cae63918d567427a4587e9
thedhruvish <thedhruvish@gmail.com>
2025-06-30 10:00:18 +0530

    init structore

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once  './../config.php';
+// db connection
+
+$conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
