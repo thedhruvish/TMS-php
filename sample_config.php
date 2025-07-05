25bf443e7a5cba4a8d53f1dbdd611aa1f3b8bb1f
thedhruvish <thedhruvish@gmail.com>
2025-07-05 12:30:31 +0530

    login with gooogle

--- sample_config.php
+++ sample_config.php
@@ -7,4 +7,8 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'tms-phps');
 
+// google auth
+define('GOOGLE_CLIENT_ID', '548770837279-ck7uoutg2hfui10lm6358kncn5vcckas.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_REDIRECT_URI', 'http://localhost/tms-php/g-callback.php');
 ?>
