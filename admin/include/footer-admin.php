64c06de1029de00172ef3680fec569826e47f820
thedhruvish <thedhruvish@gmail.com>
2025-07-05 13:26:32 +0530

    fix navbar profile

--- admin/include/footer-admin.php
+++ admin/include/footer-admin.php
@@ -30,7 +30,8 @@
 
 
   <script src="../src/plugins/src/global/vendors.min.js"></script>
-  <script src="../src/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- fix navbar profile
+  <script src="../src/bootstrap/js/bootstrap.bundle.min.js"></script>   -->
   <script src="../src/plugins/src/perfect-scrollbar/perfect-scrollbar.min.js"></script>
   <script src="../src/plugins/src/mousetrap/mousetrap.min.js"></script>
   <script src="../src/plugins/src/waves/waves.min.js"></script>
