b140f6d0861ff77c238a086c8265bcc7a3703fdb
MaunishPrajapati <prajapatimaunish52@gmail.com>
2025-07-07 14:51:56 +0530

    shop page

--- admin/products-add.php
+++ admin/products-add.php
@@ -3,6 +3,4 @@ require_once './include/header-admin.php';
 require_once './include/sidebar-admin.php';
 ?>
 
-
-
-<?php include('./include/footer-admin.php'); ?>
+<?php include_once('./include/footer-admin.php'); ?>
